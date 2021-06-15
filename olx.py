01c0cf00007b919b03b9f6757330c78fdea23e92
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-14 21:59:43 -0600

    using WebDriverWait

--- olx.py
+++ olx.py
@@ -1,27 +1,29 @@
-import random
-from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome('chromedriver.exe')
 
 driver.get('https://www.olx.com.ec/autos_c378')
 
-loadMoreButton = driver.find_element_by_xpath('//button[@data-aut-id="btnLoadMore"]')
-
 for _ in range(3):
     try:
+        loadMoreButton = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//button[@data-aut-id="btnLoadMore"]'))
+        )
         loadMoreButton.click()
-        sleep(random.uniform(6, 8))
-        loadMoreButton = driver.find_element_by_xpath('//button[@data-aut-id="btnLoadMore"]')
-    except:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//li[@data-aut-id="itemBox"]//span[@data-aut-id="itemTitle"]'))
+        )
+    except Exception as e:
+        print(e)
         break
 
 autos = driver.find_elements_by_xpath('//li[@data-aut-id="itemBox"]')
-i = 0
+
 for auto in autos:
-    i+=1
-    print(i)
     price = auto.find_element_by_xpath('.//span[@data-aut-id="itemPrice"]').text
     details = auto.find_element_by_xpath('.//span[@data-aut-id="itemDetails"]').text
     title = auto.find_element_by_xpath('.//span[@data-aut-id="itemTitle"]').text
-    print(price, details, title)
+    print(price, details, title, sep=' | ')
